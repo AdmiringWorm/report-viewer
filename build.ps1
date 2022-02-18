721682e14937b865ce6b2a6feae656209d91924e
AdmiringWorm <kim.nordmo@gmail.com>
2022-02-18 21:02:45 +0100

    build: change directory seperators

--- build.ps1
+++ build.ps1
@@ -2,8 +2,8 @@ param(
     [switch]$Release
 )
 
-$distDir = "$PSScriptRoot\dist"
-$srcDir = "$PSScriptRoot\src"
+$distDir = "$PSScriptRoot/dist"
+$srcDir = "$PSScriptRoot/src"
 
 yarn install
 
@@ -13,19 +13,19 @@ if ($Release) {
         Remove-Item "$distDir\*" -Recurse
     }
     yarn tsc --removeComments --stripInternal --noEmitOnError
-    yarn uglifyjs "$distDir\template.js" --compress --mangle -o "$distDir\template.min.js"
-    $sha = Get-FileHash -Algorithm SHA256 -Path "$distDir\template.min.js" | % Hash
+    yarn uglifyjs "$distDir/template.js" --compress --mangle -o "$distDir/template.min.js"
+    $sha = Get-FileHash -Algorithm SHA256 -Path "$distDir/template.min.js" | % Hash
     $scriptRef = "<script src=`"template.min.js?sha=$sha`"></script>"
 } else {
     yarn tsc --incremental --sourceMap
-    $sha = Get-FileHash -Algorithm SHA256 -Path "$distDir\template.js" | % Hash
+    $sha = Get-FileHash -Algorithm SHA256 -Path "$distDir/template.js" | % Hash
     $scriptRef = "<script src=`"template.js?sha=$sha`"></script>"
 }
-$htmlContent = Get-Content -Encoding utf8 "$srcDir\index.html" | % {
+$htmlContent = Get-Content -Encoding utf8 "$srcDir/index.html" | % {
     $_ -replace "{{SCRIPT_REFERENCE}}", $scriptRef
 }
 
-$htmlContent | Out-File -Encoding utf8 "$distDir\index.html"
+$htmlContent | Out-File -Encoding utf8 "$distDir/index.html"
 
 $scripts = Get-ChildItem src -Filter "*.ps1" -Recurse
 
@@ -34,4 +34,4 @@ $newScriptContent = $scripts | % {
     ""
 }
 
-$newScriptContent | Out-File "$distDir\helpers.ps1"
+$newScriptContent | Out-File "$distDir/helpers.ps1"
